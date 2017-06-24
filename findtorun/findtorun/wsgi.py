27573eb4a72774f9b8f838eaad750e163dff765f
sampath <sampath.surineni@oracle.com>
2017-06-24 16:25:07 +0530

    modified wsgi

--- findtorun/findtorun/wsgi.py
+++ findtorun/findtorun/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+import site
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "findtorun.settings")
+try:
+    site.addsitedir('/home/ubuntu/envfind/lib/python3.5/site-packages')
+    sys.path.append('/home/ubuntu/find-to-run/findtorun')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "findtorun.settings")
+    activate_env = os.path.expanduser('/home/ubuntu/envfind/bin/activate_this.py')
+    exec(compile(open(activate_env).read(),activate_env,'exec'), dict(__file__=activate_env))
+except Exception as e:
+    print('Exception occured')
+    print(e)
+    sys.exit(-1)
+
 
 application = get_wsgi_application()
