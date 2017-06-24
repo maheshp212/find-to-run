73be71242cfa0594e03dacc9270812a661b665a8
mahesh <mahesh.perla@gritinnovatino.com>
2017-06-24 21:54:58 +0530

    maehsh perla :: api calling is done

--- webapp/modules/home/home-controller.js
+++ webapp/modules/home/home-controller.js
@@ -1,8 +1,23 @@
 'use strict';
 
-appModule.controller('homeCtrl', function($scope, $location) {
-	$scope.a = AppUtil.gethexa(16);
-	$scope.login = function(){
-		alert("perla");
-	};
+appModule.controller('homeCtrl', function($scope, $location, $http) {
+	$scope.logging = function () {
+    	var url = 'http://api.findtorun.fun/signup';
+      	var data = {
+		    "email": $scope.email,
+		    "first_name": $scope.first_name,
+		    "last_name": $scope.last_name,
+		    "city": $scope.city
+		};
+
+       	$http({
+           method : "GET",
+           url : url,
+           data: data
+       }).then(function(response) {
+           $scope.msg = response.data;
+       }, function(response) {
+           $scope.msg = response.statusText;
+       });
+  	}
 });
