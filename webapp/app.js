d03f14b2b409ade19e827ca8638983d12266970a
mahesh <mahesh.perla@gritinnovatino.com>
2017-06-24 15:43:39 +0530

    Multiple pages are added

--- webapp/app.js
+++ webapp/app.js
@@ -18,5 +18,17 @@ appModule.config(function($routeProvider, $locationProvider) {
         templateUrl : "modules/home/home.html",
         controller: "homeCtrl"
     })
+    .when("/login", {
+        templateUrl : "modules/login/login.html",
+        controller: "loginCtrl"
+    })
+    .when("/add-location", {
+        templateUrl : "modules/add-location/add-location.html",
+        controller: "addLocationCtrl"
+    })
+    .when("/participate", {
+        templateUrl : "modules/participate/participate.html",
+        controller: "participateCtrl"
+    })
     
 });
