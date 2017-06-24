11a6c9f964f37fa36aaf8ff648c5c77c98dc3782
mahesh <mahesh.perla@gritinnovatino.com>
2017-06-24 15:19:27 +0530

    Mahesh Perla:: final page

--- webapp/app.js
+++ webapp/app.js
@@ -3,20 +3,14 @@
 var appModule = angular.module('sample-app', [
         'ngRoute',
         'ngMaterial',
-        'ngMdIcons'
+        
     ]);
 
 // app.constant('appConst', {
 //         PROJECT_MANAGER: 'Project Manager'
 //     });
 
-appModule.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
-     $mdThemingProvider.enableBrowserColor({
-      theme: 'myTheme', // Default is 'default'
-      palette: 'accent', // Default is 'primary', any basic material palette and extended palettes are available
-      hue: '200' // Default is '800'
-    });
-
+appModule.config(function($routeProvider, $locationProvider) {
 
     $locationProvider.hashPrefix('');
     $routeProvider
