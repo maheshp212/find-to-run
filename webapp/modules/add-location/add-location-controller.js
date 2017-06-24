975f8e45b941217d907414603d5861360179c0f5
mahesh <mahesh.perla@gritinnovatino.com>
2017-06-24 17:34:37 +0530

    Mahessh Perla :: aded the google maps

--- webapp/modules/add-location/add-location-controller.js
+++ webapp/modules/add-location/add-location-controller.js
@@ -1,5 +1,11 @@
 'use strict';
 
-appModule.controller('addLocationCtrl', function($scope, $location) {
-	$scope.a = AppUtil.gethexa(16);
+appModule.controller('addLocationCtrl', function($scope, $location, NgMap) {
+	$scope.latlng = [-25.363882,131.044922];
+	$scope.getpos = function(event){
+		$scope.latlng = [event.latLng.lat(), event.latLng.lng()];
+	};
+
+
+	
 });
