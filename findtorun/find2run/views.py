53aee2464157ae846e025fcfbe77968823fb8cb0
sampath <sampath.surineni@oracle.com>
2017-06-25 11:23:18 +0530

    done geting all locations

--- findtorun/find2run/views.py
+++ findtorun/find2run/views.py
@@ -4,8 +4,6 @@ import logging
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
-from utilities.utils import get_request_info
-
 from find2run.models import User, Location
 
 logger = logging.getLogger(__name__)
@@ -67,14 +65,37 @@ def get_locations(request):
 
 def get_users_in_location(request):
     request_info = request.GET
-    users = User.objects.filter(lattitude=request_info['lattitude'],
-                                longitude=request_info['longitude'])
-    users_in_loc = []
 
-    for a_user in users:
-        user_dict = {}
-        user_dict['email'] = a_user.email
-        user_dict['first_name'] = a_user.first_name
-        user_dict['last_name'] = a_user.last_name
-        users_in_loc.append(user_dict)
+    users_in_loc = {}
+    all_locations = Location.objects.filter(
+        lattitude=request_info['lattitude'],
+        longitude=request_info['longitude'])
+
+    for a_location in all_locations:
+        this_user = a_location.email.email
+        if this_user not in users_in_loc.keys():
+            users_in_loc[this_user] = {}
+            users_in_loc[this_user]['locations'] = []
+            all_user_locations = a_location.email.location_set.all()
+
+            for a_user_loc in all_user_locations:
+                users_in_loc[this_user]['locations'].append(
+                    [a_user_loc.lattitude,
+                    a_user_loc.longitude])
+        # this_loc = [a_location['lattitude'], a_location['longitude']]
+        # all_locations.append(this_loc)
+        
+    # for a_user in users:
+    #     user_dict = {}
+    #     user_dict['email'] = a_user.email
+    #     user_dict['first_name'] = a_user.first_name
+    #     user_dict['last_name'] = a_user.last_name
+    #     users_in_loc.append(user_dict)
+
+    #     user_dict['locations'] = []
+
+    #     all_locations = Location.objects.filter(email=a_user)
+    #     for a_location in all_locations:
+    #         this_loc = [a_location['lattitude'], a_location['longitude']]
+    #         all_locations.append(this_loc)
     return HttpResponse(json.dumps(users_in_loc))
