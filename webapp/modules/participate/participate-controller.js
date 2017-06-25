c65b7342beac7db4f605e78868bd96c66e0882a1
mahesh <mahesh.perla@gritinnovatino.com>
2017-06-25 11:50:41 +0530

    Mahesh Perla:: fianl page

--- webapp/modules/participate/participate-controller.js
+++ webapp/modules/participate/participate-controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-appModule.controller('participateCtrl', function($scope, $location, NgMap,$http, $mdDialog) {
+appModule.controller('participateCtrl', function($scope, $location, NgMap,$http, $mdDialog, $window, $rootScope, $mdToast) {
 
 	NgMap.getMap().then(function(map) {
 		$scope.map = map;
@@ -45,6 +45,42 @@ appModule.controller('participateCtrl', function($scope, $location, NgMap,$http,
 	};
 	$scope.addUserToLocation = function(ev, user) {
 		console.log(user);
+
+
+		var confirm = $mdDialog.confirm()
+		.title('Join the run')
+		.textContent('You have poked the runner ' + user.first_name + ' ' + user.last_name + 'successfully')
+		.ariaLabel('Lucky day')
+		.targetEvent(ev)
+		.ok('Join')
+		.cancel('Cancel');
+
+		$mdDialog.show(confirm).then(function() {
+			$scope.status = 'You decided to get rid of your debt.';
+			var email = $rootScope.lemail;
+			var lattitude = $scope.latlng[0].toFixed(3);
+			var longitude = $scope.latlng[1].toFixed(3);
+			var url = 'http://api.findtorun.fun/update_location?email=' + email + '&lattitude=' + lattitude+ '&longitude=' + longitude;
+
+			$http({
+				method : "GET",
+				url : url,
+			}).then(function(response) {
+				$scope.msg = response.data;
+				$mdToast.show(
+					$mdToast.simple()
+					.textContent('Marked Location successfully added!')
+					.position('bottom right')
+					.hideDelay(3000)
+					);
+				$window.location.href = '#/participate';
+				$scope.latlng = [];
+			}, function(response) {
+				$scope.msg = response.statusText;
+			});
+		}, function() {
+		});
+/*
 		$mdDialog.show(
       	$mdDialog.alert()
         .clickOutsideToClose(true)
@@ -53,6 +89,6 @@ appModule.controller('participateCtrl', function($scope, $location, NgMap,$http,
         .ariaLabel('Alert Dialog Demo')
         .ok('Got it!')
         .targetEvent(ev)
-    );
-	};
+        );*/
+    };
 });
